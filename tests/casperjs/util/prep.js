2e2d839fa0ea5ba5511524da1eef6de32cec59ea
Bert Pareyn <pareyn.bert@gmail.com>
2013-07-25 14:43:10 +0100

    Refactor for 1.1 upgrade

--- tests/casperjs/util/prep.js
+++ tests/casperjs/util/prep.js
@@ -1,22 +1,24 @@
-casper.echo('Prepare environment for tests');
-
 // Set up test tenant
-casper.start('http://admin.oae.com', function() {
-	casper.then(function() {
-		userUtil().doAdminLogIn('administrator', 'administrator');
-	});
+casper.test.begin('Prepare environment for tests', function(test) {
+	casper.start('http://admin.oae.com', function() {
+		casper.waitForSelector('#admin-login-form', function() {
+			casper.then(function() {
+				userUtil().doAdminLogIn('administrator', 'administrator');
+			});
 
-	casper.then(function() {
-		adminUtil().createTenant('test', 'CasperJS Tenant', 'test.oae.com', function() {
-			adminUtil().writeConfig('test', {
-				'oae-principals/recaptcha/enabled': false
-			}, function() {
-				userUtil().doAdminLogOut();
+			casper.then(function() {
+				adminUtil().createTenant('test', 'CasperJS Tenant', 'test.oae.com', function() {
+					adminUtil().writeConfig('test', {
+						'oae-principals/recaptcha/enabled': false
+					}, function() {
+						userUtil().doAdminLogOut();
+					});
+				});
 			});
 		});
 	});
-});
 
-casper.run(function() {
-	casper.test.done();
+	casper.run(function() {
+		test.done();
+	});
 });
