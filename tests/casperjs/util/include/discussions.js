f8ccb2605221f4c4f601e4f19acac079e933c960
Simone <simone_love_tigers@live.nl>
2013-08-14 11:56:41 +0100

    notifications completed (two tests: added to group and created a discussion)

--- tests/casperjs/util/include/discussions.js
+++ tests/casperjs/util/include/discussions.js
@@ -13,32 +13,125 @@ var discussionUtil = function() {
      * @param  {Function}     callback               Standard callback function
      * @param  {Discussion}   callback.discussion    The created discussion object
      */
-    var createDiscussion = function(callback) {
-        var discussion = null;
+    var createDiscussion = function(numToCreate, callback) {
+        var toCreate = numToCreate || 1;
+        var discussions = [];
+
+        casper.start('http://test.oae.com/').repeat(toCreate, function() {
+            var rndString = mainUtil().generateRandomString();
+            data = casper.evaluate(function(rndString) {
+                return JSON.parse(__utils__.sendAJAX('/api/discussion/create', 'POST', {
+                    'displayName': 'Discussion ' + rndString,
+                    'description': 'Talk about all the things!',
+                    'visibility': 'public'
+                }, false));
+            }, rndString);
+
+            casper.then(function() {
+                if (data) {
+                    casper.echo('Created \'Discussion' + rndString + '\'.');
+                    discussions.push(data);
+                } else {
+                    casper.echo('Could not create discussion \'Discussion' + rndString + '\'.', 'ERROR');
+                }
+            });
+        });
+
+        casper.then(function() {
+            callback(discussions);
+        });
+    };
+    /*
+    var createGroupDiscussion = function(groupname) {
+        casper.start('http://test.oae.com/me/discussions', function() {
+            var rndString = mainUtil().generateRandomString();
+            casper.waitForSelector('#lhnavigation-page', function() {
+                casper.echo('test');
+                casper.click('.oae-trigger-creatediscussion');
+                casper.waitForSelector('form#creatediscussion-form', function() {
+                    casper.captureSelector('discussions3.png','#creatediscussion-permissions');
+                    casper.echo('form');
+                    casper.fill('form#creatediscussion-form', {
+                        'creatediscussion-name': rndString,
+                        'creatediscussion-topic': 'Talk about all the things!'
+                    }, false);
+                    casper.click('#creatediscussion-change-permissions');
+                    casper.captureSelector('discussions5.png','#creatediscussion-change-permissions');
+                    //TOT HIER GAAT HET GOED, HET FORM WORDT INGEVULD
+                    //HIERNA WORDT OF DE NIET KNOP INGEDRUKT OF HET NIEUWE SCHERM NIET INGELADEN
+                    casper.wait(1000, function() {
+                        casper.capture('discussions4.png');
+                        casper.test.assertExists('#creatediscussion-permissions-container[style="display: none"]');
+                        casper.test.assertExists('#creatediscussion-permissions-container[style="display: block"]');
+                        casper.test.assertExists('#creatediscussion-permissions-container[display="none"]');
+                        casper.test.assertExists('#creatediscussion-permissions-container[display="block"]');
+                        casper.test.assertExists('#creatediscussion-permissions-container');
+                        casper.test.assertVisible('#creatediscussion-permissions-container');
+                        casper.test.assertNotVisible('#creatediscussion-permissions-container');
+                    });
+                    casper.waitUntilVisible('#creatediscussion-permissions-container', function() {
+                        casper.capture('discussions.png');
+                        casper.echo('list');
+                        casper.evaluate(function(groupname) {
+                            document.querySelector('#setpermissions-container .as-selections input').value = groupname;
+                        }, groupname);
+                        casper.capture('discussions2.png');
+                        casper.click('#setpermissions-container .as-selections input');
+                        casper.waitForSelector('.as-list li', function() {
+                            casper.echo('save');
+                            casper.click('.as-list li');
+                            casper.click('#setpermissions-savepermissions');
+                            casper.waitForSelector('#creatediscussion-create', function() {
+                                casper.echo('create');
+                                casper.click('#creatediscussion-create');
+                            });
+                        });
+                    });
+                });
+            });
+        });
+    };*/
+
+
+    /**
+     * Creates a discussion in the given groupUrl
+     *
+     * @param   {String}    groupUrl    The group.profilePath of the group you want to add a discussion.
+     */
+    var createGroupDiscussion = function(groupUrl, callback) {
         var rndString = mainUtil().generateRandomString();
-        data = casper.evaluate(function(rndString) {
-            return JSON.parse(__utils__.sendAJAX('/api/discussion/create', 'POST', {
-                'displayName': 'Discussion ' + rndString,
-                'description': 'Talk about all the things!',
-                'visibility': 'public'
-            }, false));
-        }, rndString);
-
+        casper.thenOpen('http://test.oae.com' + groupUrl + '/discussions', function() {
+            // Need to fix this, but it doesn't work without the wait, it wil timeout without it
+            casper.wait(5000,function() {
+                casper.waitForSelector('.oae-trigger-creatediscussion', function() {
+                    casper.click('.oae-trigger-creatediscussion');
+                    casper.waitForSelector('#creatediscussion-create', function() {
+                        casper.then(function() {
+                            casper.fill('form#creatediscussion-form', {
+                                'creatediscussion-name': 'Discussion ' + rndString,
+                                'creatediscussion-topic': 'Talk about all the things!'
+                            }, false);
+                        });
+                        casper.then(function() {
+                            casper.click('#creatediscussion-create');
+                            casper.waitForSelector('.oae-clip-content', function() {
+                                casper.echo('created discussion \'Discussion ' + rndString + '\' in group with path \'' + groupUrl + '\'');
+                                });
+                            });
+                        });
+                    });
+                });
+            });
+        });
+                
         casper.then(function() {
-            if (data) {
-                casper.echo('Created \'Discussion' + rndString + '\'.');
-                discussion = data;
-            } else {
-                casper.echo('Could not create discussion \'Discussion' + rndString + '\'.', 'ERROR');
-            }
+            callback(rndString);
         });
 
-        casper.then(function() {
-            callback(discussion);
-        });
     };
 
     return {
-        'createDiscussion': createDiscussion
+        'createDiscussion': createDiscussion,
+        'createGroupDiscussion': createGroupDiscussion
     };
 };
--- tests/casperjs/util/include/groups.js
+++ tests/casperjs/util/include/groups.js
@@ -46,8 +46,85 @@ var groupUtil = function() {
         });
     };
 
+    /**
+     * Help function to open the manageAccess of a group
+     */
+    var openManageAccess = function() {
+        casper.waitForSelector('#group-clip-container .oae-clip-content button', function(){
+            casper.click('#group-clip-container .oae-clip-content button');
+            casper.waitForSelector('button.group-trigger-manageaccess', function() {
+                casper.click('button.group-trigger-manageaccess');
+            });
+        });
+    };
+
+    /**
+     * Add the given user to the given group by using the manageaccess screen.
+     *
+     * @param   {String}    username    The name of the user you want to add to the group
+     * @param   {String}    groupUrl    The group.profilePath of the group you want to use
+     */
+    var addUserToGroup = function(username, groupUrl) {
+        casper.thenOpen('http://test.oae.com'+groupUrl, function() {
+            openManageAccess();
+            casper.waitForSelector('#manageaccess-share-add-more', function() {
+                casper.click('#manageaccess-share-add-more');
+                casper.waitForSelector('.as-selections', function() {
+                    casper.evaluate(function(username) {
+                        document.querySelector('#manageaccess-share .as-selections input').value = username;
+                        document.querySelector('#manageaccess-share-role').value = 'manager';
+                    }, username);
+        
+                    // Click on the input field to make the autosuggest list load
+                    casper.click('#manageaccess-share .as-selections input');
+        
+                    // Wait for the auto suggest list for the users
+                    casper.waitForSelector('.as-list li', function() {
+                        // Click the first user (the one we want)
+                        casper.click('.as-list li');
+                        // Click the add button
+                        casper.click('#manageaccess-share-update');
+        
+                        // Wait for the save button to load
+                        casper.waitForSelector('#manageaccess-overview-save', function() {
+                            // Save the user to the group
+                            casper.click('#manageaccess-overview-save');
+                            casper.echo('Added user ' + username + ' to group as manager');
+                        });
+                    });
+                });
+            });
+        });
+    };
+
+    /**
+    * Change the visibility of this group
+    * @param   {String}    type    The type of visibility you want to have
+    *                              can be 'private', 'public', 'loggedin' everything else will be public
+    * @param   {Object}    group   The group you want to change the visibility from
+    */
+    var changeVisibility = function(type, group) {
+        casper.thenOpen('http://test.oae.com'+group.profilePath, function() {
+            openManageAccess();
+            casper.click('#manageaccess-change-visibility');
+            casper.waitForSelector('#manageaccess-visibility', function() {
+                if(type === 'private' || type === 'public' || type === 'loggedin') {
+                    casper.click('#oae-visibility-'+type);
+                } else {
+                    casper.click('#oae-visibility-public');
+                }
+                casper.click('#manageaccess-visibility-save');
+                casper.waitForSelector('#manageaccess-overview-save', function() {
+                    casper.click('#manageaccess-overview-save');
+                });
+            });
+        });
+    };
+
     return {
         'createGroup': createGroup,
-        'createdGroups': createdGroups
+        'createdGroups': createdGroups,
+        'addUserToGroup': addUserToGroup,
+        'changeVisibility': changeVisibility
     };
 };
